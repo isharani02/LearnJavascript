0ffc672b303eca79d0e4803c6d3d9a513797292f
Isha Rani <ishuisharani@gmail.com>
2025-11-27 21:13:49 +0530

    Changes

--- 01_Basics/08_dateTime.js
+++ 01_Basics/08_dateTime.js
@@ -3,3 +3,15 @@
 
 let myDate = new Date();
 console.log(myDate);
+
+//toDateString()  :  Returns only date.
+console.log("toDateString() returns Current Date : ", myDate.toDateString());
+
+//toTimeString()  :   Returns only time.
+console.log("toTimeString() returns only time : ", myDate.toTimeString());
+
+//toLocaleDateString()  :   Returns localize date.
+console.log("toLocaleDateString() returns localize date : ", myDate.toLocaleDateString());
+
+//toLocalTimeString()   :   Returns localize time.
+console.log("toLocalTimeString() returns localize time : ", myDate.toLocalTimeString());
