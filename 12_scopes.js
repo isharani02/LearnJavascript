2915065ce930a471057431192a54ce1e252c4577
Isha Rani <ishuisharani@gmail.com>
2025-12-03 23:41:13 +0530

    Changes

--- 12_scopes.js
+++ 12_scopes.js
@@ -41,7 +41,45 @@ if(true){
 console.log("Outside of block : ");
 console.log("num1 : ", num1);
 console.log("num2 : ", num2);
-console.log("num3 : ", num3);       // 30  <-- overwritten by var inside block
+console.log("num3 : ", num3);       // 30  --> overwritten by var inside block
 // console.log("num4 : ", num4);
 
-//**Closure Function**
+//**Closure Function Example**
+function outer(){
+    const username = "Isha";         // Variable defined in outer function
+
+    function inner(){
+        const website = "you-tube";    
+       // inner() can access outer() variables (closure)
+        console.log(username);
+    }
+    //console.log(website);   // Error: website is only available inside inner()
+
+    inner();    // Calling inner function
+}
+outer();        // Calling outer function
+
+
+//Lexical scope --> Inner scope can access outer scope, but outer scope cannot access inner scope.
+
+if(true){
+    let userName = "Isha";
+    if(userName === "Isha"){
+        let website = "you-tube";
+        console.log(website + userName);        //Inner block can access outer block variables
+    }
+   //console.log(website);        //Error: website is not accessible outside the inner block
+};
+//console.log(userName);           //Error: userName is not accessible outside the outer block
+
+console.log(addOne(5));        // Direct declared function can be used before declaration
+
+function addOne (num){
+    return (num + 1);
+};
+
+//console.log(addTwo(10));    // Function declared in a variable cannot be used before declaration
+const addTwo = function(num){
+    return (num + 1);
+};
+console.log(addTwo(10));
