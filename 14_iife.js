ca3e5d38cef7d08a1ff1a08687ab1aecfaeea2ab
Isha Rani <ishuisharani@gmail.com>
2025-12-04 01:03:53 +0530

    Changes

---
+++ 14_iife.js
@@ -0,0 +1,28 @@
+//** IIFE in JavaScript **
+//----------------------------------------------------------
+
+// IIFE (Immediately Invoked Function Expression) 
+// A function in JavaScript that runs immediately after it is created. 
+// To avoid polluting the global scope.
+
+
+//Simple function declaration(Not an IIFE)
+//------------------------------------
+function dbSQL(){
+    console.log(`DB CONNECTED.`);
+};
+
+//IIFE
+//-------------------------------
+
+//Note:  Wrap whole function in a paranthesis and execute by giving paranthesis at the end with semicolon.
+
+ //Named IIFE
+(function dbMongo(){
+    console.log(`DB Connected.`);
+})();
+
+// Arrow function IIFE with parameter
+((name)=>{
+    console.log(`DB Connected Two ${name}`);
+})("Isha");
