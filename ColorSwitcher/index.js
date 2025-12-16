decb5124f1aef7da8b36fb36962eea26456b4883
Isha Rani <ishuisharani@gmail.com>
2025-12-16 22:19:57 +0530

    Changes

---
+++ ColorSwitcher/index.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+buttons.forEach(function(button){
+
+    console.log(button);
+    button.addEventListener("click",function(e){
+        console.log(e);
+        console.log(e.target);
+        body.style.backgroundColor = e.target.id;
+        // if(e.target.id === 'grey'){
+        //     body.style.backgroundColor = e.target.id;
+        // }
+        // if(e.target.id === 'lightgreen'){
+        //     body.style.backgroundColor = e.target.id;
+        // }
+        // if(e.target.id === 'orange'){
+        //     body.style.backgroundColor = e.target.id;
+        // }
+        // if(e.target.id === 'darkblue'){
+        //     body.style.backgroundColor = e.target.id;
+        // }
+        // if(e.target.id === 'purple'){
+        //     body.style.backgroundColor = e.target.id;
+        // }
+        // if(e.target.id === 'lightpink'){
+        //     body.style.backgroundColor = e.target.id;
+        // }
+    });
+});    
